064eeecf07afe532dc30c4aa6170de6c1b0dc9a1
Martin Larios <mrlarios_1994@hotmail.es>
2022-01-14 18:57:45 -0600

    :package: adding tailwindCSS and axios to fetch GitHub API

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./src/**/*.js",
+    "./src/**/*.ts",
+    "./src/**/*.jsx",
+    "./src/**/*.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
