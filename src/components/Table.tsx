4845c3d9f3d9b10bc6f7fcad6f0ef4d23ccd2f44
Martin Larios <mrlarios_1994@hotmail.es>
2022-01-16 09:15:21 -0600

    :construction: adding Back button in Repos Page

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { HeaderTable } from "../helpers/headerTable";
 import { ReposInfo } from "../interfaces/ReposInterface";
 import HeaderTableInformation from "./HeaderTableInformation";
@@ -9,6 +10,7 @@ interface Props {
 }
 
 const Table: React.FC<Props> = ({ reposInfo }) => {
+  const navigate = useNavigate();
   return (
     <div className="bg-white p-8 rounded-md w-full">
       <div className=" flex items-center justify-between pb-6">
@@ -18,9 +20,20 @@ const Table: React.FC<Props> = ({ reposInfo }) => {
           </h2>
           <span className="text-xs">Repositories Information</span>
         </div>
-        <div className="flex flex-row items-center gap-2 border-2 border-cyan-200 p-2">
-         <div className="w-10 h-10 bg-red-100 border-white border-2" />
-          <span className="text-xs font-semibold">Current Repository</span>
+        <div className="flex flex-col">
+          <div className="self-end mb-2">
+            <button
+              className="bg-blue-400 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              type="button"
+              onClick={() => navigate("/")}
+            >
+              Back
+            </button>
+          </div>
+          <div className="flex flex-row items-center gap-2 border-2 border-cyan-200 p-2">
+            <div className="w-10 h-10 bg-red-100 border-white border-2" />
+            <span className="text-xs font-semibold">Current Repository</span>
+          </div>
         </div>
       </div>
       <div>
